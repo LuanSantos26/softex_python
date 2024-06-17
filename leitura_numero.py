788dddc1c1887d8ae474409414766633509e4aad
Lucas-E-Softex <lucaspoppy911@gmail.com>
2024-06-17 20:51:18 -0300

    Create leitura_numero.py

---
+++ leitura_numero.py
@@ -0,0 +1,12 @@
+numero = int(input("digite um numeto que seja meno ou igual a 1000:"));
+if numero >= 1000:
+ print("o numero tem que ser menor que 1000");
+else:
+   centenas=numero//100
+   resto=numero % 100
+   dezenas=numero //10
+   unidades= resto % 10
+
+print("centenas",centenas);
+print("dezenas", dezenas);
+print("unidades", unidades);
