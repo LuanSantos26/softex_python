2caa1e96ed3ddd0d549cba2434d4bdad4bcf8720
Luan Feitosa Santos <140123122+LuanSantos26@users.noreply.github.com>
2024-06-26 21:35:55 -0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,4 +1,11 @@
+CONSTANTE=3.14 # Em python não existe constante, apenas um acordo de boas praticas entre programadores, toda variavel escrita com caps lock é uma constante.
 idade=25
 nome="Luan"
 print(f'meu nome é {nome} e eu tenho {idade} anos')
 # o f serve para concatenar uma frase inteira.
+print(int(idade))
+preco="10.50"
+print(float(preco))
+print(nome,idade)
+print(nome,idade, end= "...\n")# esse exemplo serve para quebra de linha.
+print(nome,idade, sep= "#") # a hashtag serve como separador.
