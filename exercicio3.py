9142f3deb4301161c222d14f59bf39eda54945b9
denisfilho <140123122+LuanSantos26@users.noreply.github.com>
2024-07-04 20:52:40 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,14 +1,14 @@
-numeros = []
-for i in range(5):
-    numero = float(input(f"Digite o {i+1}º número: "))
-    numeros.append(numero)
+def main():
+     quantidades=5
+     soma=0
+     for i in range (quantidades):
+         numero=int(input(f"digite o {i+1} numero: "))
+         soma=soma+numero
+         media=soma/quantidades
 
-soma = sum(numeros)
-media = soma / len(numeros)
-
-print(f"Soma dos números: {soma:.2f}")
-print(f"Média dos números: {media:.2f}")
-
+         print("soma:", soma)
+         print("edia:", media)
+main()
 
 
 # Solicita ao usuário para digitar 5 números
@@ -25,3 +25,4 @@ for num in numeros:
 
 # Exibe o maior número
 print(f"O maior número é: {maior_numero:.2f}")
+
